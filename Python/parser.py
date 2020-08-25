f2e4f8120a46684f7308687418e55b9bc127a510
lassa97 <flassai@gmail.com>
2020-08-26 00:15:19 +0200

    Create the parser for IPv4, ICMP, TCP and UDP frames

---
+++ Python/parser.py
@@ -0,0 +1,45 @@
+import binascii
+import socket
+import struct
+
+class Parser:
+    def __init__(self):
+        pass
+
+    def ethernet_header(self, frame, ethernet):
+        data = struct.unpack("!6s6sH", frame)
+        ethernet.destination = binascii.hexlify(data[0]).decode('utf-8')
+        ethernet.destination = (':'.join(format(c, '02x') for c in bytes.fromhex(ethernet.destination))).upper()
+        ethernet.source = binascii.hexlify(data[1]).decode('utf-8')
+        ethernet.source = (':'.join(format(c, '02x') for c in bytes.fromhex(ethernet.source))).upper()
+        ethernet.ethertype = hex(data[2])
+
+    def icmp_header(self, frame, icmp):
+        data = struct.unpack("!BBH4s", frame)
+        icmp.type = data[0]
+        icmp.code = data[1]
+        icmp.rest = binascii.hexlify(data[3]).decode('utf-8')
+
+    def ipv4_header(self, frame, ipv4):
+        data = struct.unpack("!BBHHHBBH4s4s", frame)
+        ipv4.version = (data[0] & 240) >> 4
+        ipv4.total_length = data[2]
+        ipv4.TTL = data[5]
+        ipv4.protocol = data[6]
+        ipv4.source = socket.inet_ntoa(data[8])
+        ipv4.destination = socket.inet_ntoa(data[9])
+
+    def tcp_header(self, frame, tcp):
+        data = struct.unpack("!HH4s4sHHHH", frame)
+        tcp.source = data[0]
+        tcp.destination = data[1]
+        tcp.sequence_number = str(int(binascii.hexlify(data[2]), 16))
+        tcp.ACK = str(int(binascii.hexlify(data[3]), 16))
+        tcp.flags = data[4] & 511
+        tcp.window_size = data[5]
+
+    def udp_header(self, frame, udp):
+        data = struct.unpack("!HHHH", frame)
+        udp.source = data[0]
+        udp.destination = data[1]
+        udp.length = data[2]
